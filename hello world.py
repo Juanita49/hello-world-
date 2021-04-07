dd792b1937325df0bd143c0b5330313a68f101ec
Juanita49 <82115925+Juanita49@users.noreply.github.com>
2021-04-08 00:03:06 +0530

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,6 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:22:44) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #myfirstprogram
+>>> print('hello world!')
+hello world!
+>>> 
